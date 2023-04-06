4935c3690ef3dd3a3d91c110e6876660522cc2ed
Bichchuran10 <bichchuran10@gmail.com>
2023-04-06 13:22:22 +0530

    add Two numbers

---
+++ typeScript_AddTwoNumbers/app.js
@@ -0,0 +1,6 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(1, 6));
+//console.log(add("10","5")) //it will throw an error in the compile time 
+//that is why we use typeScript , to write bug free code and to avoid runtime error
---
+++ typeScript_AddTwoNumbers/app.ts
@@ -0,0 +1,9 @@
+function add(num1:number,num2:number) //set type as number
+{
+    return num1+num2
+}
+
+console.log(add(1,6))
+
+//console.log(add("10","5")) //it will throw an error in the compile time 
+//that is why we use typeScript , to write bug free code and to avoid runtime error
